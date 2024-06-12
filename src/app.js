967389a82093363fd727779305369f9a1aec5bed
WickedZ2309 <168111978+WickedZ2309@users.noreply.github.com>
2024-06-12 13:10:42 -0400

    Inect the forecast HTML from JS and adjust CSS with lesson 4 code

--- src/app.js
+++ src/app.js
@@ -56,7 +56,31 @@ function handleSearchSubmit(event) {
   seacrhCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+          <div class="weather-forecast-date">${day}</div>
+          <div class="weather-forecast-icon">🌥️</div>
+          <div class="weather-forecast-temperatures">
+            <div class="weather-forecast-temparture">
+              <strong>15°</strong>
+            </div>
+            <div class="weather-forecast-temparture">19°</div>
+          </div>
+        </div>`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 seacrhCity("Montreal");
+displayForecast();
